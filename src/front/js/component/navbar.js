fd01f494c1f5d40f6c9cf62e8278a83799fbcda1
valerialorenzo <valerialorenzo9@gmail.com>
2023-02-06 18:12:30 +0000

    menu hamburguesa

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,16 @@
-import React, { useContext }from "react";
+import React, { useContext, useState }from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 export const Navbar = () => {
   const { store } = useContext(Context);
+const [estadoNavbar,setEstadoNavbar]= useState(false);
 
-  return (
+const onChangeMenuHamburguesa=() =>
+{
+setEstadoNavbar(!estadoNavbar)
+
+}
+return (
     <nav className="navbar bg-body-tertiary" style={{ backgroundColor: "#86C8BC" }}>
       <div className="container-fluid">
         <Link to="/">
@@ -25,7 +31,7 @@ export const Navbar = () => {
               Publicar
             </a>
           </div>
-          <div className="float-right">
+          <div className="nav-item float-right">
             {store.auth ?
             <div className="nav-item">
               <a className="nav-link active ml-25px" style={{backgroundColor: "#CEEDC7", color:"#36544F"}} aria-current="page" href="#">
@@ -37,6 +43,24 @@ export const Navbar = () => {
                 Iniciar sesion
               </a>
             </div>}
+          </div>
+          <div className="nav-item">
+            <div className="dropdown">
+              <a onClick={onChangeMenuHamburguesa} className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
+
+              </a>
+              <div className="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownMenuButton" style={estadoNavbar ? { display:'block', right: '10px'}: { display:'none', right: '10px'}}>
+                <a className="dropdown-item" href="#perdidos">Perdidos</a>
+                <a className="dropdown-item" href="#encontrados">Encontrados</a>
+                <a className="dropdown-item" href="#adopcion">En adopcion</a>
+                <a className="dropdown-item" href="#donaciones">Donaciones</a>
+                {/* CUANDO SE HAGA LA VISTA PPAL, HAY QUE DIVIDIR EN SECCIONES CADA PARTE, Y PONER ID PERDIDO, ENCONTRADO... EN CADA SECCION QUE CORRESPONDA LUEGO SE TRAE POR ACA CON EL ID */}
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
