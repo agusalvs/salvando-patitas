a94f5df16ac98b1f4cb2d802712191cac4ba0daf
valerialorenzo <valerialorenzo9@gmail.com>
2023-02-07 21:31:29 +0000

    agrega import de login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState }from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import Login from "../component/login.jsx";
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
 const [estadoNavbar,setEstadoNavbar]= useState(false);
