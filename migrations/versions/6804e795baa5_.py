e6d2e0ff10956f8a49584a5d5e198642328544d4
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-02-08 15:16:24 +0000

    Endpoint para publicar

--- migrations/versions/6804e795baa5_.py
+++ migrations/versions/6804e795baa5_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: d720d1512544
+Revision ID: 6804e795baa5
 Revises: 
-Create Date: 2023-02-06 18:15:14.627603
+Create Date: 2023-02-08 14:58:11.678917
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd720d1512544'
+revision = '6804e795baa5'
 down_revision = None
 branch_labels = None
 depends_on = None
