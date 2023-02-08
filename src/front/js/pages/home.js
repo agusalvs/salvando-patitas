3ebafb7814e57a3f76e1685885746d3f9f980edd
agusalvs <agustina.alvarezsacra@gmail.com>
2023-02-08 15:31:54 +0000

    Trabajando en la vista home, agregando el componente perdidos

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Perdidos } from "../component/perdidos.jsx";
-import { Encontrados } from "../component/encontrados.jsx";
-import { Adopción } from "../component/adopcion.jsx";
+// import { Encontrados } from "../component/encontrados.jsx";
+// import { Adopción } from "../component/adopcion.jsx";
 import { Inicio } from "../component/inicio.jsx";
 import "../../styles/home.css";
 
@@ -14,14 +14,18 @@ export const Home = () => {
     <div className="container-fluid bg-white">
       {/* <!-- First Featurette --> */}
       <Inicio />
-      <h2 className="ms-5 ps-5">Characters</h2>
+      <h2 className="ms-5 ps-5">Perdidos</h2>
       <div
         className="row flex-nowrap overflow-auto mx-auto scrollbar scrollbar-black bordered-black square"
         style={{ width: "90%" }}
       >
-        {store.characters.map((props) => (
+        {store.mascotas.map((props) => (
           <div className="col">
-            <Perdidos name={props.name} id={props.uid} key={props.uid} />
+            <Perdidos
+              name={props.titulo}
+              id={props.foto1}
+              key={props.descripcion}
+            />
           </div>
         ))}
       </div>
