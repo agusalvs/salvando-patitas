08774d6ea087512d48213eb283aaec940fa94981
agusalvs <agustina.alvarezsacra@gmail.com>
2023-02-13 19:26:36 +0000

    agregue los botones de Mas info y Ver mas con sus respectivas funcionalidades, centre las tarjetas

--- src/front/js/component/mascotas.jsx
+++ src/front/js/component/mascotas.jsx
@@ -1,30 +1,19 @@
 import React, { useState } from "react";
-// import { useContext } from "react";
-// import { Link } from "react-router-dom";
-// import { Context } from "../store/appContext.js";
+import { Link } from "react-router-dom";
 
 export const Mascotas = (props) => {
-  // const { store, actions } = useContext(Context);
-
   return (
     <>
-      {/* <div className="card" style={{ width: "18rem" }}>
-        <img src={props.foto1} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{props.titulo}</h5>
-          <p className="card-text">{props.descripcion}</p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div> */}
-
       <div
-        className="card mb-3"
+        className="card mb-3 py-2"
         style={{
-          width: "18rem",
-          backgroundColor: "#CEEDC7",
+          width: "20rem",
+          // height: "30rem",
+          backgroundColor: props.backgroundColor,
+          borderColor: props.borderColor,
+          borderWidth: "4px",
           borderRadius: "1rem",
+          color: "#36544F",
         }}
       >
         <div className="card-header bg-transparent text-center">
@@ -35,86 +24,20 @@ export const Mascotas = (props) => {
         <img src={props.foto1} className="card-img-top" alt="..." />
         {/* </div> */}
         <div className="card-footer bg-transparent justify-content-center">
-          <p className="card-text">{props.descripcion}</p>
-          {/* <a href="/perdidos" className="btn btn-light">
+          <p className="card-text text-truncate">{props.descripcion}</p>
+          <a
+            href={"/perdidos/" + props.id}
+            className="btn btn-light"
+            style={{
+              color: "white",
+              backgroundColor: "#36544F",
+              opacity: "25%",
+            }}
+          >
             Más información
-          </a> */}
+          </a>
         </div>
       </div>
     </>
-    // <div
-    //   className="container-fluid p-5"
-    //   id="perdidos"
-    //   style={{ backgroundColor: "#FFF6BD" }}
-    // >
-    //   <h1>Perdidos</h1>
-    //   <p>
-    //     Donec ullancorper nulla non metus auctur frangilla. Bestibulum id ligula
-    //     porta felis euismod semper. Praesent commodo cursus magna, vel
-    //     scelerisque nisl consecutor. Fusque dabisus , tellus ac cursus commodo.
-    //   </p>
-    //   <div className="row">
-    //     <div className="col-4">
-    //       <div className="card" style={{ width: "25rem" }}>
-    //         <img
-    //           src="https://cdn.pixabay.com/photo/2020/01/18/09/05/network-4774766__340.jpg"
-    //           className="card-img-top"
-    //           alt="..."
-    //         />
-    //         <div className="card-body">
-    //           <h5 className="card-title" style={{ color: "rgb(56, 120, 146)" }}>
-    //             Project Name
-    //           </h5>
-    //           <p className=" card-text">
-    //             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-    //             harum debitis, iusto non pariatur, culpa quae corporis eius enim
-    //             eaque tempora fugiat ratione cupiditate minima sint! At illum
-    //             amet corporis.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="col-4">
-    //       <div className="card" style={{ width: "25rem" }}>
-    //         <img
-    //           src="https://cdn.pixabay.com/photo/2020/01/18/09/05/network-4774766__340.jpg"
-    //           className="card-img-top"
-    //           alt="..."
-    //         />
-    //         <div className="card-body">
-    //           <h5 className="card-title" style={{ color: "rgb(56, 120, 146)" }}>
-    //             Project Name
-    //           </h5>
-    //           <p className="card-text">
-    //             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-    //             harum debitis, iusto non pariatur, culpa quae corporis eius enim
-    //             eaque tempora fugiat ratione cupiditate minima sint! At illum
-    //             amet corporis.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="col-4">
-    //       <div className="card" style={{ width: "25rem" }}>
-    //         <img
-    //           src="https://cdn.pixabay.com/photo/2020/01/18/09/05/network-4774766__340.jpg"
-    //           className="card-img-top"
-    //           alt="..."
-    //         />
-    //         <div className="card-body">
-    //           <h5 className="card-title" style={{ color: "rgb(56, 120, 146)" }}>
-    //             Project Name
-    //           </h5>
-    //           <p className="card-text">
-    //             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-    //             harum debitis, iusto non pariatur, culpa quae corporis eius enim
-    //             eaque tempora fugiat ratione cupiditate minima sint! At illum
-    //             amet corporis.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,75 +6,87 @@ import { Mascotas } from "../component/mascotas.jsx";
 // import { Encontrados } from "../component/encontrados.jsx";
 // import { Adopción } from "../component/adopcion.jsx";
 // import Login from "../component/login.jsx";
-import { Footer } from "../component/footer.jsx";
-import { Footerhome } from "../component/footerhome.jsx";
-import { Link } from "react-router-dom";
+// import { Footer } from "../component/footer.jsx";
+// import { Footerhome } from "../component/footerhome.jsx";
+// import { Link } from "react-router-dom";
 import "../../styles/home.css";
-import { element } from "prop-types";
+// import { element } from "prop-types";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  const params = useParams();
+  // const params = useParams();
 
   useEffect(() => {
     actions.mascotasHome();
   }, []);
 
-  let perdidos = store.mascotas.filter(
+  const perdidos = store.mascotas.filter(
     (mascota) => mascota.estado == "Perdido"
   );
 
-  let encontrados = store.mascotas.filter(
+  const encontrados = store.mascotas.filter(
     (mascota) => mascota.estado == "Encontrado"
+  );
+
+  const adopcion = store.mascotas.filter(
+    (mascota) => mascota.estado == "En adopción"
   );
 
   return (
     // <!-- Page Content -->
-    <div className="container-fluid bg-white">
+    <div className="mx-auto">
       {/* <!-- First Featurette --> */}
       <Inicio />
 
       <div
-        className="container-fluid py-5"
-        id="portfolio"
+        className="container-fluid py-5 text-center justify-content-center align-items-center mx-auto"
+        id="perdidos"
         style={{ backgroundColor: "#FFF6BD" }}
       >
-        <h1>Perdidos</h1>
-        <p>
+        <h1 style={{ color: "#36544F" }}>Perdidos</h1>
+        <p style={{ color: "#36544F" }}>
           En esta sección podrás ver todos los animalitos que se encuentran
           actualmente perdidos, encontrados o en adopción.
         </p>
-        {/* <p>
-          En esta sección podrás ver todos los animalitos que se encuentran
-          actualmente perdidos. Cualquier información que desees aportar, puedes
-          ponerte en contacto con quien hizo la publicación ingresando en "más
-          información".
-        </p> */}
-        {/* <div className="row">
-          <div className="col-4"> */}
         <div
-          className="row flex-nowrap overflow-auto mx-auto scrollbar scrollbar-black bordered-black round"
+          className="row mx-auto justify-content-center flex-nowrap overflow-auto scrollbar scrollbar-black bordered-black round"
           style={{ width: "90%" }}
         >
           {perdidos.map((item) => (
-            <div className="col">
+            <div className="col mx-auto d-flex justify-content-center align-items-center">
               <Mascotas
+                backgroundColor={"#CEEDC7"}
                 titulo={item?.titulo}
                 descripcion={item.descripcion}
                 foto1={item.foto1}
                 key={item}
+                id={item.id}
               />
             </div>
           ))}
         </div>
+        <div className="card-footer bg-transparent justify-content-center">
+          <a
+            href={"/perdidos"}
+            className="btn btn-light mt-4"
+            style={{
+              width: "160px",
+              color: "white",
+              backgroundColor: "#36544F",
+              opacity: "50%",
+            }}
+          >
+            Ver más
+          </a>
+        </div>
       </div>
       <div
-        className="container-fluid py-5"
-        id="portfolio"
-        style={{ backgroundColor: "#FFF6BD" }}
+        className="container-fluid py-5 text-center"
+        id="encontrados"
+        style={{ backgroundColor: "#FFD4B2" }}
       >
-        <h1>Encontrados</h1>
-        <p>
+        <h1 style={{ color: "#36544F" }}>Encontrados</h1>
+        <p style={{ color: "#36544F" }}>
           En esta sección podrás ver todos los animalitos que se encuentran
           actualmente encontrados, encontrados o en adopción.
         </p>
@@ -91,15 +103,81 @@ export const Home = () => {
           style={{ width: "90%" }}
         >
           {encontrados.map((item) => (
-            <div className="col">
+            <div className="col mx-auto d-flex justify-content-center align-items-center">
               <Mascotas
+                backgroundColor={"#FFF6BD"}
                 titulo={item?.titulo}
                 descripcion={item.descripcion}
                 foto1={item.foto1}
                 key={item}
+                id={item.id}
               />
             </div>
           ))}
+        </div>
+        <div className="card-footer bg-transparent justify-content-center">
+          <a
+            href={"/perdidos"}
+            className="btn btn-light mt-4"
+            style={{
+              width: "160px",
+              color: "white",
+              backgroundColor: "#36544F",
+              opacity: "50%",
+            }}
+          >
+            Ver más
+          </a>
+        </div>
+      </div>
+      <div
+        className="container-fluid py-5 text-center"
+        id="adopcion"
+        style={{ backgroundColor: "#CEEDC7" }}
+      >
+        <h1 style={{ color: "#36544F" }}>En adopción</h1>
+        <p style={{ color: "#36544F" }}>
+          En esta sección podrás ver todos los animalitos que se encuentran
+          actualmente encontrados, encontrados o en adopción.
+        </p>
+        {/* <p>
+          En esta sección podrás ver todos los animalitos que se encuentran
+          actualmente perdidos. Cualquier información que desees aportar, puedes
+          ponerte en contacto con quien hizo la publicación ingresando en "más
+          información".
+        </p> */}
+        {/* <div className="row">
+          <div className="col-4"> */}
+        <div
+          className="row flex-nowrap overflow-auto mx-auto scrollbar scrollbar-black bordered-black square"
+          style={{ width: "90%" }}
+        >
+          {adopcion.map((item) => (
+            <div className="col mx-auto d-flex justify-content-center align-items-center">
+              <Mascotas
+                backgroundColor={"#FFD4B2"}
+                titulo={item?.titulo}
+                descripcion={item.descripcion}
+                foto1={item.foto1}
+                key={item}
+                id={item.id}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="card-footer bg-transparent justify-content-center">
+          <a
+            href={"/perdidos"}
+            className="btn btn-light mt-4"
+            style={{
+              width: "160px",
+              color: "white",
+              backgroundColor: "#36544F",
+              opacity: "50%",
+            }}
+          >
+            Ver más
+          </a>
         </div>
       </div>
     </div>
