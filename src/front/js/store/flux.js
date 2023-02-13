fb0b6d0c3172f60b8f44f4e0137763320ba342c0
MelissaOcampo <melissa.ocampo.2001@gmail.com>
2023-02-13 17:01:31 +0000

    cambiar contraseña

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,210 +1,210 @@
 import Swal from "sweetalert2";
 
-const getState = ({
-    getStore,
-    getActions,
-    setStore
-}) => {
-    return {
-        store: {
-            message: null,
-            demo: [{
-                    title: "FIRST",
-                    background: "white",
-                    initial: "white",
-                },
-                {
-                    title: "SECOND",
-                    background: "white",
-                    initial: "white",
-                },
-            ],
-            Swal: require("sweetalert2"),
+const getState = ({ getStore, getActions, setStore }) => {
+  return {
+    store: {
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
         },
-        actions: {
-            // Use getActions to call a function within a fuction
-
-            signup: (
-                userNombre,
-                userCorreo,
-                userCelular,
-                userContraseña,
-                userDireccion
-            ) => {
-                fetch(
-                        "https://3001-agusalvs-salvandopatita-yp2yoipd64w.ws-us85.gitpod.io/api/registro", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                // 'Content-Type': 'application/x-www-form-urlencoded',
-                            },
-                            body: JSON.stringify({
-                                nombre: userNombre,
-                                email: userCorreo,
-                                celular: userCelular,
-                                contraseña: userContraseña,
-                                direccion: userDireccion,
-                            }), // body data type must match "Content-Type" header
-                        }
-                    )
-                    .then((response) => {
-                        console.log(response.status);
-                        if (response.status === 201) {
-                            Swal.fire({
-                                position: "middle",
-                                icon: "success",
-                                title: "Te registraste correctamente",
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                            setStore({
-                                auth: true,
-                            });
-                        }
-                        return response.json();
-                    })
-                    .then((data) => {
-                        console.log(data);
-                        if (data.msg === "Bad email or password") {
-                            alert(data.msg);
-                        }
-                        localStorage.setItem("token", data.access_token);
-                    })
-                    .catch((err) => console.log(err));
-            },
-
-            login: (userEmail, userPassword) => {
-                fetch(
-                        "https://3001-agusalvs-salvandopatita-aarew83zv9d.ws-us85.gitpod.io/api/autenticacion", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                // 'Content-Type': 'application/x-www-form-urlencoded',
-                            },
-                            body: JSON.stringify({
-                                email: userEmail,
-                                contraseña: userPassword,
-                            }), // body data type must match "Content-Type" header
-                        }
-                    )
-                    .then((response) => {
-                        console.log(response.status);
-                        if (response.status === 200) {
-                            // setStore({
-                            //     showModal: "show"
-                            // });
-                            Swal.fire({
-                                position: "middle",
-                                icon: "success",
-                                title: "Has iniciado sesión",
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                            setStore({
-                                auth: true,
-                            });
-                        }
-                        return response.json();
-                    })
-                    .then((data) => {
-                        console.log(data);
-                        if (data.msg === "Usuario o contraseña incorrectos") {
-                            Swal.fire({
-                                icon: "error",
-                                title: "Opa, qué rompimo'?...",
-                                text: data.msg,
-                                // footer: '<a href="">Why do I have this issue?</a>'
-                            })(data.msg);
-                        } else if (data.msg === "Necesitas registrarte") {
-                            Swal.fire({
-                                title: data.msg,
-                                showClass: {
-                                    popup: "animate__animated animate__fadeInDown",
-                                },
-                                hideClass: {
-                                    popup: "animate__animated animate__fadeOutUp",
-                                },
-                            });
-                        }
-                        localStorage.setItem("token", data.access_token);
-                    })
-                    .catch((err) => console.log(err));
-            },
-
-            //FALTA CAMBIAR LA URL
-
-            recuperar: (userNuevacontraseña, userRepetircontraseña) => {
-                fetch(
-                        "https://3001-agusalvs-salvandopatita-yp2yoipd64w.ws-us85.gitpod.io/api/recuperar", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                // 'Content-Type': 'application/x-www-form-urlencoded',
-                            },
-                            body: JSON.stringify({
-                                nuevacontraseña: userNuevacontraseña,
-                                repetircontraseña: userRepetircontraseña,
-                            }), // body data type must match "Content-Type" header
-                        }
-                    )
-                    .then((response) => {
-                        console.log(response.status);
-                        if (response.status === 201) {
-                            Swal.fire({
-                                position: "middle",
-                                icon: "success",
-                                title: "Contraseña cambiada correctamente",
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                            setStore({
-                                auth: true,
-                            });
-                        }
-                        return response.json();
-                    })
-                    .then((data) => {
-                        console.log(data);
-                        if (data.msg === "Bad email or password") {
-                            alert(data.msg);
-                        }
-                        localStorage.setItem("token", data.access_token);
-                    })
-                    .catch((err) => console.log(err));
-            },
-
-            getMessage: async () => {
-                try {
-                    // fetching data from the backend
-                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-                    const data = await resp.json();
-                    setStore({
-                        message: data.message,
-                    });
-                    // don't forget to return something, that is how the async resolves
-                    return data;
-                } catch (error) {
-                    console.log("Error loading message from backend", error);
-                }
-            },
-            changeColor: (index, color) => {
-                //get the store
-                const store = getStore();
-
-                //we have to loop the entire demo array to look for the respective index
-                //and change its color
-                const demo = store.demo.map((elm, i) => {
-                    if (i === index) elm.background = color;
-                    return elm;
-                });
-
-                //reset the global store
-                setStore({
-                    demo: demo,
-                });
-            },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
         },
-    };
+      ],
+      Swal: require("sweetalert2"),
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+
+      signup: (
+        userNombre,
+        userCorreo,
+        userCelular,
+        userContraseña,
+        userDireccion
+      ) => {
+        fetch(
+          "https://3001-agusalvs-salvandopatita-yp2yoipd64w.ws-us85.gitpod.io/api/registro",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+              nombre: userNombre,
+              email: userCorreo,
+              celular: userCelular,
+              contraseña: userContraseña,
+              direccion: userDireccion,
+            }), // body data type must match "Content-Type" header
+          }
+        )
+          .then((response) => {
+            console.log(response.status);
+            if (response.status === 201) {
+              Swal.fire({
+                position: "middle",
+                icon: "success",
+                title: "Te registraste correctamente",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              setStore({
+                auth: true,
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            if (data.msg === "Bad email or password") {
+              alert(data.msg);
+            }
+            localStorage.setItem("token", data.access_token);
+          })
+          .catch((err) => console.log(err));
+      },
+
+      login: (userEmail, userPassword) => {
+        fetch(
+          "https://3001-agusalvs-salvandopatita-aarew83zv9d.ws-us85.gitpod.io/api/autenticacion",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+              email: userEmail,
+              contraseña: userPassword,
+            }), // body data type must match "Content-Type" header
+          }
+        )
+          .then((response) => {
+            console.log(response.status);
+            if (response.status === 200) {
+              // setStore({
+              //     showModal: "show"
+              // });
+              Swal.fire({
+                position: "middle",
+                icon: "success",
+                title: "Has iniciado sesión",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              setStore({
+                auth: true,
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            if (data.msg === "Usuario o contraseña incorrectos") {
+              Swal.fire({
+                icon: "error",
+                title: "Opa, qué rompimo'?...",
+                text: data.msg,
+                // footer: '<a href="">Why do I have this issue?</a>'
+              })(data.msg);
+            } else if (data.msg === "Necesitas registrarte") {
+              Swal.fire({
+                title: data.msg,
+                showClass: {
+                  popup: "animate__animated animate__fadeInDown",
+                },
+                hideClass: {
+                  popup: "animate__animated animate__fadeOutUp",
+                },
+              });
+            }
+            localStorage.setItem("token", data.access_token);
+          })
+          .catch((err) => console.log(err));
+      },
+
+      //FALTA CAMBIAR LA URL
+
+      recuperar: (userNuevacontraseña, userRepetircontraseña) => {
+        fetch(
+          "https://3001-agusalvs-salvandopatita-9r75lu9au08.ws-us86.gitpod.io/api/recuperar",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+              nuevacontraseña: userNuevacontraseña,
+              repetircontraseña: userRepetircontraseña,
+            }), // body data type must match "Content-Type" header
+          }
+        )
+          .then((response) => {
+            console.log(response.status);
+            if (response.status === 201) {
+              Swal.fire({
+                position: "middle",
+                icon: "success",
+                title: "Contraseña cambiada correctamente",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              setStore({
+                auth: true,
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            if (data.msg === "Bad email or password") {
+              alert(data.msg);
+            }
+            localStorage.setItem("token", data.access_token);
+          })
+          .catch((err) => console.log(err));
+      },
+
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({
+            message: data.message,
+          });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({
+          demo: demo,
+        });
+      },
+    },
+  };
 };
 
 export default getState;
