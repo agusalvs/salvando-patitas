f0448528ba6f43d63681003ef3a32eefe400a896
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-02-23 18:27:57 +0000

    Mapa funcional, guardando la ubicacion en la store

--- src/front/js/component/mapa.jsx
+++ src/front/js/component/mapa.jsx
@@ -31,68 +31,67 @@ export const Mapa = () => {
     console.log(localizacion);
   }
 
-  // function LocationMarker() {
-  var myIcon = L.icon({
-    iconUrl:
-      "https://res.cloudinary.com/de1k9ojw2/image/upload/v1677005762/Salvando%20Patitas/Salvando_Patitas_9479452d783b1ae2605a4547577e1313-removebg-preview_ygxh4j-b_rgb_86c8bc_jxhbij.png",
-    iconSize: [38, 45],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: "",
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94],
-  });
   const [position, setPosition] = useState(null);
+  function LocationMarker() {
+    var myIcon = L.icon({
+      iconUrl:
+        "https://res.cloudinary.com/de1k9ojw2/image/upload/v1677005762/Salvando%20Patitas/Salvando_Patitas_9479452d783b1ae2605a4547577e1313-removebg-preview_ygxh4j-b_rgb_86c8bc_jxhbij.png",
+      iconSize: [38, 45],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowUrl: "",
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94],
+    });
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+    const onMove = useCallback(() => {
+      setPosition(map.getCenter());
+    }, [map]);
 
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-  const onMove = useCallback(() => {
-    setPosition(map.getCenter());
-  }, [map]);
-
-  useEffect(() => {
-    map.on("move", onMove);
-    return () => {
-      map.off("move", onMove);
-    };
-  }, [map, onMove]);
-  // setLocalizacion(position);
-  // return position === null ? null : (
-  // <Marker
-  //   position={position}
-  //   // onClick={(e) =>
-  //   //   setLocalizacion(position.lat.toFixed(4), position.lng.toFixed(4))
-  //   // }
-  //   style={{
-  //     backgroundImage:
-  //       "https://res.cloudinary.com/de1k9ojw2/image/upload/v1677005762/Salvando%20Patitas/Salvando_Patitas_9479452d783b1ae2605a4547577e1313-removebg-preview_ygxh4j-b_rgb_86c8bc_jxhbij.png",
-  //   }}
-  //   icon={myIcon}
-  // >
-  //   <Popup>
-  //     <span
-  //       onClick={() =>
-  //         setLocalizacion({
-  //           latitud: position.lat.toFixed(4),
-  //           longitud: position.lng.toFixed(4),
-  //         })
-  //       }
-  //     >
-  //       {">>Enviar<<"}
-  //     </span>{" "}
-  //     latitude: {position.lat.toFixed(4)}, longitude:{" "}
-  //     {position.lng.toFixed(4)}{" "}
-  //   </Popup>
-  // </Marker>
-  // );
-  // }
+    useEffect(() => {
+      map.on("move", onMove);
+      return () => {
+        map.off("move", onMove);
+      };
+    }, [map, onMove]);
+    setLocalizacion(position);
+    return position === null ? null : (
+      <Marker
+        position={position}
+        // onClick={(e) =>
+        //   setLocalizacion(position.lat.toFixed(4), position.lng.toFixed(4))
+        // }
+        style={{
+          backgroundImage:
+            "https://res.cloudinary.com/de1k9ojw2/image/upload/v1677005762/Salvando%20Patitas/Salvando_Patitas_9479452d783b1ae2605a4547577e1313-removebg-preview_ygxh4j-b_rgb_86c8bc_jxhbij.png",
+        }}
+        icon={myIcon}
+      >
+        <Popup>
+          <span
+            onClick={() =>
+              setLocalizacion({
+                latitud: position.lat.toFixed(4),
+                longitud: position.lng.toFixed(4),
+              })
+            }
+          >
+            {">>Enviar<<"}
+          </span>{" "}
+          latitude: {position.lat.toFixed(4)}, longitude:{" "}
+          {position.lng.toFixed(4)}{" "}
+        </Popup>
+      </Marker>
+    );
+  }
 
   // function ultimaposi(ev) {
   //   setPosition(ev.latlng); // ev is an event object (MouseEvent in this case)
@@ -110,32 +109,7 @@ export const Mapa = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker
-          position={position}
-          // onClick={(e) =>
-          //   setLocalizacion(position.lat.toFixed(4), position.lng.toFixed(4))
-          // }
-          style={{
-            backgroundImage:
-              "https://res.cloudinary.com/de1k9ojw2/image/upload/v1677005762/Salvando%20Patitas/Salvando_Patitas_9479452d783b1ae2605a4547577e1313-removebg-preview_ygxh4j-b_rgb_86c8bc_jxhbij.png",
-          }}
-          icon={myIcon}
-        >
-          <Popup>
-            <span
-              onClick={() =>
-                setLocalizacion({
-                  latitud: position.lat.toFixed(4),
-                  longitud: position.lng.toFixed(4),
-                })
-              }
-            >
-              {">>Enviar<<"}
-            </span>{" "}
-            latitude: {position.lat.toFixed(4)}, longitude:{" "}
-            {position.lng.toFixed(4)}{" "}
-          </Popup>
-        </Marker>
+        <LocationMarker />
       </MapContainer>
       <button
         onClick={(e) =>
